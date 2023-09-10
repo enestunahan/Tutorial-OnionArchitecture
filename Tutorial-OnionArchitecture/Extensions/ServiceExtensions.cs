064200ba96a1750a7d6e081a158aa4c5f907ca66
Enes Tunahan <enesstunahan@gmail.com>
2023-09-10 22:59:09 +0300

    Cors configuration

---
+++ Tutorial-OnionArchitecture/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Tutorial_OnionArchitecture.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+        }
+    }
+}
