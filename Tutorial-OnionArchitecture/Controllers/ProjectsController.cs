cb4ae3a37803c5df0e4d9930c44e20e38024ca3c
Enes Tunahan <enesstunahan@gmail.com>
2023-09-11 21:04:06 +0300

    Default gelen controller silindi , entitylerin tutulacağı yeni katman eklendi  , yeni controller oluşturulup denemelik endpoint yazıldı

---
+++ Tutorial-OnionArchitecture/Controllers/ProjectsController.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial_OnionArchitecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private List<Project> _projectList;
+        public ProjectsController()
+        {
+            _projectList = new List<Project>
+            {
+                new Project{Id = Guid.NewGuid() , Name = "Project 1"},
+                new Project{Id = Guid.NewGuid() , Name = "Project 2"},
+                new Project{Id = Guid.NewGuid() , Name = "Project 3"}
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_projectList);
+        }
+    }
+}
