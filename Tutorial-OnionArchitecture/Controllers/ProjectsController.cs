c5105ff66b7003f33157d730a890fc8245dccaae
Enes Tunahan <enesstunahan@gmail.com>
2023-09-11 21:33:52 +0300

    Built-in olarak gelen ILogger ifadesi incelendi

--- Tutorial-OnionArchitecture/Controllers/ProjectsController.cs
+++ Tutorial-OnionArchitecture/Controllers/ProjectsController.cs
@@ -8,7 +8,8 @@ namespace Tutorial_OnionArchitecture.Controllers
     public class ProjectsController : ControllerBase
     {
         private List<Project> _projectList;
-        public ProjectsController()
+        private ILogger<ProjectsController> _logger;    
+        public ProjectsController(ILogger<ProjectsController> logger)
         {
             _projectList = new List<Project>
             {
@@ -16,12 +17,27 @@ namespace Tutorial_OnionArchitecture.Controllers
                 new Project{Id = Guid.NewGuid() , Name = "Project 2"},
                 new Project{Id = Guid.NewGuid() , Name = "Project 3"}
             };
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_projectList);
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;  
+
+                _logger.LogInformation("Project.Get() has been run.");
+                return Ok(_projectList);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Project.Get() has been crashed"+ ex.Message);
+                throw;
+            }
+          
         }
     }
 }
